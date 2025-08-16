234e612747a7b6cdd17f2a0345093fa71ad46e88
saadieabdullah-cmd <saadie.abdullah@gmail.com>
2025-08-16 15:32:38 +0500

    Update dgm_dashboard7.py

--- dgm_dashboard7.py
+++ dgm_dashboard7.py
@@ -5,6 +5,7 @@ import plotly.express as px
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 from pathlib import Path
+import request
 import base64
 import os
 
@@ -564,5 +565,6 @@ if __name__ == "__main__":
         layout="wide"
     )
     main()
+
 
 
